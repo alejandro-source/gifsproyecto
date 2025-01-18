ba61e0becb18e71b922fa919e33c6bedbad3d57f
alejandro-source <alejnadro36@gmail.com>
2025-01-18 17:01:18 +0100

    Subiendo cambios a la nueva rama

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -4,11 +4,12 @@ import { GifsService } from '../../../gifs/services/gifs.service';
 @Component({
   selector: 'app-sidebar',
   standalone: false,
-  
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  public currentTag: string = ''; // Agregar esta propiedad
+
   constructor(private gifsService: GifsService) {}
 
   get tags() {
@@ -16,10 +17,12 @@ export class SidebarComponent {
   }
 
   searchTag(tag: string) {
+    this.currentTag = tag; // Marca el tag seleccionado
     this.gifsService.searchTag(tag);
   }
 
   clearHistory() {
-    this.gifsService.clearHistory(); // Llama al servicio para limpiar el historial
+    this.gifsService.clearHistory();
+    this.currentTag = ''; // Limpia el tag activo
   }
 }
